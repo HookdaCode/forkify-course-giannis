abfa4b26339152ed202c33c4e2b0c3c7f3bedc41
HookdaCode <giannisfourikis@gmail.com>
2025-02-07 20:37:54 +0200

    fraction fix v3

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -130,7 +130,7 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ele.quantity ? numberToFraction(ele.quantity).toString() : ''
+            ing.quantity ? numberToFraction(ing.quantity).toString() : ''
             // ing.quantity ? new Fraction(ing.quantity).toString() : ''
           }</div>
           <div class="recipe__description">
